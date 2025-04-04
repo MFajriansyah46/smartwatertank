dd518ed867d8fa95630aaf006dfe118eeff51415
MFajriasyah46 <mfajriansyah23@mhs.politala.ac.id>
2025-04-05 07:09:14 +0800

    perbaikan autentikasi

--- app/src/main/java/com/bangraja/smartwatertank/MainActivity.java
+++ app/src/main/java/com/bangraja/smartwatertank/MainActivity.java
@@ -1,19 +1,32 @@
 package com.bangraja.smartwatertank;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private BottomNavigationView bottomNavigationView;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Inisialisasi Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        // Cek apakah user sudah login
+        if (mAuth.getCurrentUser() == null) {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            finish();
+            return;
+        }
 
         // Inisialisasi toolbar
         toolbar = findViewById(R.id.toolbar);
