7a5870d671bb250f7d9554e69138625f0a55c9a0
aldi <aldiand84@gmail.com>
2025-04-08 10:18:53 +0800

    menukar posisi authentikasi

--- app/src/main/java/com/bangraja/smartwatertank/MainActivity.java
+++ app/src/main/java/com/bangraja/smartwatertank/MainActivity.java
@@ -17,8 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
 
         // Inisialisasi Firebase Auth
         mAuth = FirebaseAuth.getInstance();
@@ -29,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
             finish();
             return;
         }
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         // Inisialisasi toolbar
         toolbar = findViewById(R.id.toolbar);
