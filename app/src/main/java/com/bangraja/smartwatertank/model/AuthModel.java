4958efa46b0ca974e4f8d6c9df23ebcd6abae827
MFajriasyah46 <mfajriansyah23@mhs.politala.ac.id>
2025-04-25 02:11:21 +0800

    profile

--- app/src/main/java/com/bangraja/smartwatertank/model/AuthModel.java
+++ app/src/main/java/com/bangraja/smartwatertank/model/AuthModel.java
@@ -2,12 +2,15 @@ package com.bangraja.smartwatertank.model;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class AuthModel {
     private final FirebaseAuth auth;
-
+    private final CollectionReference profileRef;
     public AuthModel() {
         auth = FirebaseAuth.getInstance();
+        profileRef = FirebaseFirestore.getInstance().collection("tb_profile");
     }
 
     public FirebaseAuth getAuth() {
@@ -16,5 +19,14 @@ public class AuthModel {
 
     public FirebaseUser getCurrentUser() {
         return auth.getCurrentUser();
+    }
+
+    public String getEmail() {
+        FirebaseUser user = getCurrentUser();
+        return user != null ? user.getEmail() : null;
+    }
+
+    public CollectionReference getProfile() {
+        return profileRef;
     }
 }
