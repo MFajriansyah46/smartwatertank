b8ba3e0990a600fe3a1e2f8a0ea40a6526d371b9
Muhammad Mujayhid Noor Rizqe <mujayhid22@gmail.com>
2025-04-25 23:59:10 +0800

    penambahan notifikasi buka keran otomatis

--- app/src/main/java/com/bangraja/smartwatertank/view/SettingsActivity.java
+++ app/src/main/java/com/bangraja/smartwatertank/view/SettingsActivity.java
@@ -10,28 +10,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.bangraja.smartwatertank.R;
 import com.bangraja.smartwatertank.controller.AuthController;
 import com.bangraja.smartwatertank.controller.CommandController;
+import com.bangraja.smartwatertank.controller.NotificationController;
 
 public class SettingsActivity extends AppCompatActivity {
     private TextView emailTextView;
     private Switch bukaKeranOtomatis;
     private Button logoutBtn;
+
     private AuthController ac;
     private CommandController cc;
+    private NotificationController nc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        // Inisialisasi controller
         ac = new AuthController();
         cc = new CommandController();
+        nc = NotificationController.getInstance();
 
+        // Hubungkan dengan layout
         emailTextView = findViewById(R.id.emailView);
         bukaKeranOtomatis = findViewById(R.id.bukaKeranOtomatis);
         logoutBtn = findViewById(R.id.logoutBtn);
 
+        // Tampilkan email
         ac.displayUserEmail(emailTextView);
+
+        // Setup switch
         cc.autoSwitch(bukaKeranOtomatis, this);
+
+        // Tambahkan listener untuk notif ketika user nyalakan/matikan mode otomatis
+        bukaKeranOtomatis.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                nc.sendNotification("Mode otomatis aktif: Saat ini anda sedang berada dalam mode pengisian otomatis.");
+            } else {
+                nc.sendNotification("Mode otomatis dimatikan: Anda telah keluar dari mode pengisian otomatis.");
+            }
+        });
+
+        // Logout button
         logoutBtn.setOnClickListener(view -> ac.logout(SettingsActivity.this));
     }
 }
