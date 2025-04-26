286e592311f7b276867a598d8250bbdc53cda4c8
MFajriasyah46 <mfajriansyah23@mhs.politala.ac.id>
2025-04-26 15:39:37 +0800

    animasi buka keran

--- app/src/main/java/com/bangraja/smartwatertank/view/DashboardFragment.java
+++ app/src/main/java/com/bangraja/smartwatertank/view/DashboardFragment.java
@@ -4,9 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -16,13 +14,13 @@ import androidx.fragment.app.Fragment;
 import com.bangraja.smartwatertank.R;
 import com.bangraja.smartwatertank.controller.CommandController;
 import com.bangraja.smartwatertank.controller.DashboardController;
-import com.bangraja.smartwatertank.controller.MonitoringController;
 import com.bangraja.smartwatertank.model.CommandModel;
 import com.bangraja.smartwatertank.model.TransmiterModel;
 
 public class DashboardFragment extends Fragment {
     private TextView pressure, height, waterVolume, progressPercent;
     private Switch bukaKeran;
+    private LinearLayout switchContainer;
     private ProgressBar progressVolume;
     private View view, riverEffect;
 
@@ -37,10 +35,11 @@ public class DashboardFragment extends Fragment {
         progressVolume = view.findViewById(R.id.progressVolume);
         progressPercent = view.findViewById(R.id.progressPercent);
         bukaKeran = view.findViewById(R.id.bukaKeran);
+        switchContainer = view.findViewById(R.id.switchContainer);
         riverEffect = view.findViewById(R.id.riverEffect);
 
         new DashboardController(new TransmiterModel()).realtimeData(pressure, height, waterVolume,progressVolume, progressPercent);
-        new CommandController(new CommandModel()).manualSwitch(bukaKeran, riverEffect);
+        new CommandController(new CommandModel()).manualSwitch(bukaKeran, riverEffect, switchContainer);
 
         return view;
     }
